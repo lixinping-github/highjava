a413b19d6f1fb6fb1a8599530f5cd0e46eab5ccd
joueur <lxp775399418@163.com>
2025-03-12 13:17:20 +0800

    main

---
+++ src/threads1/li/xin/ping/Test.java
@@ -0,0 +1,10 @@
+package threads1.li.xin.ping;
+
+public class Test {
+
+    @org.junit.Test
+    public void Tests(){
+        TestThread t1 = new TestThread();
+        t1.starts();
+    }
+}
--- src/threads1/li/xin/ping/TestThread.java
+++ src/threads1/li/xin/ping/TestThread.java
@@ -1,5 +1,22 @@
 package threads1.li.xin.ping;
+import java.lang.Thread;
+import java.lang.Runnable;
 
-public class TestThread {
+
+public class TestThread extends Thread {
+    private Runnable target;
+    @Override
+    public void run() {
+        if (target != null) {
+            target.run();
+        }
+    } //重写run方法
+    public void starts(){
+        for (int i=0;i<100;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
 
 }
