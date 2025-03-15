fc2abfdafe549cb6493a7b94900f53e80bcc9594
joueur <lxp775399418@163.com>
2025-03-15 18:46:26 +0800

    main

--- src/threads1/li/xin/ping/Test.java
+++ src/threads1/li/xin/ping/Test.java
@@ -2,9 +2,44 @@ package threads1.li.xin.ping;
 
 public class Test {
 
-    @org.junit.Test
-    public void Tests(){
-        TestThread t1 = new TestThread();
+//run subject at main thread
+    public static void main(String... args){
+         Thread t1= new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+
+                for(int i=0;i<100;i++){
+                    if(i%2 == 0){
+                        System.out.println(Thread.currentThread().getName()+"~"+i);
+                        setName("lixinping1");
+                    }
+                }
+            }
+        };
         t1.start();
+
+        Thread t2= new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                this.setPriority(7);
+                for(int i=0;i<100;i++){
+                    if(i%2 == 0){
+                        System.out.println(Thread.currentThread().getName()+"~"+i);
+                        setName("lixinping2");
+                    }
+                }
+            }
+        };
+        t2.start();
+        //注释
+
+    }
+   
+    public static void Test1(){
+
     }
 }
--- src/threads1/li/xin/ping/TestThread.java
+++ src/threads1/li/xin/ping/TestThread.java
@@ -5,12 +5,14 @@ import java.lang.Runnable;
 
 public class TestThread extends Thread {
     private Runnable target;
+
+
     @Override
     public void run() {
+        super.run();
         for (int i=0;i<100;i++){
             if(i%2 == 0){
-
-                System.out.println(i);
+                System.out.println(Thread.currentThread().getName()+"~"+i);
             }
         }
     } //重写run方法
