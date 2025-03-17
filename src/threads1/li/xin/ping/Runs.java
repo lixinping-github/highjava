025cb75e73f8e21c4582ecccfd656db46b12f8d3
joueur <lxp775399418@163.com>
2025-03-17 12:36:33 +0800

    main

---
+++ src/threads1/li/xin/ping/Runs.java
@@ -0,0 +1,23 @@
+package threads1.li.xin.ping;
+import static java.lang.System.*;
+public class Runs implements Runnable{
+    private int count = 100;
+    public void run(){
+
+        while(count!=0){
+            synchronized(this){
+                notifyAll();
+            if(count!=0){
+
+                count--;
+                out.println(Thread.currentThread().getName()+"卖出了1张票，现在还剩"+count+"张票");
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        }
+    }
+}
--- src/threads1/li/xin/ping/Test.java
+++ src/threads1/li/xin/ping/Test.java
@@ -4,38 +4,52 @@ public class Test {
 
 //run subject at main thread
     public static void main(String... args){
-         Thread t1= new Thread(){
-            @Override
-            public void run() {
-                super.run();
-
-
-                for(int i=0;i<100;i++){
-                    if(i%2 == 0){
-                        System.out.println(Thread.currentThread().getName()+"~"+i);
-                        setName("lixinping1");
-                    }
-                }
-            }
-        };
-        t1.start();
-
-        Thread t2= new Thread(){
-            @Override
-            public void run() {
-                super.run();
-
-                this.setPriority(7);
-                for(int i=0;i<100;i++){
-                    if(i%2 == 0){
-                        System.out.println(Thread.currentThread().getName()+"~"+i);
-                        setName("lixinping2");
-                    }
-                }
-            }
-        };
-        t2.start();
+//         Thread t1= new Thread(){
+//            @Override
+//            public void run() {
+//                super.run();
+//
+//
+//                for(int i=0;i<100;i++){
+//                    if(i%2 == 0){
+//                        System.out.println(Thread.currentThread().getName()+"~"+i);
+//                        setName("lixinping1");
+//                    }
+//                }
+//            }
+//        };
+//        t1.start();
+//
+//        Thread t2= new Thread(){
+//            @Override
+//            public void run() {
+//                super.run();
+//
+//                this.setPriority(7);
+//                for(int i=0;i<100;i++){
+//                    if(i%2 == 0){
+//                        System.out.println(Thread.currentThread().getName()+"~"+i);
+//                        setName("lixinping2");
+//                    }
+//                }
+//            }
+//        };
+//        t2.start();
         //,注释
+//    ThreadTest1 t1=new ThreadTest1();
+//    Thread t2=new Thread(t1);
+//    t2.start();
+//        threefifteenwork th1=new threefifteenwork();
+//        th1.start();
+//        threefifteenwork th2=new threefifteenwork();
+//        th2.start();
+//        threefifteenwork th3=new threefifteenwork();
+//        th3.start();
+        Runs r1=new Runs();
+        Thread t1=new Thread(r1);
+        Thread t2=new Thread(r1);
+        t1.start();
+        t2.start();
 
     }
    
