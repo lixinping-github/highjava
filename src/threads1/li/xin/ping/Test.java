c2fef066f361a388594fd496ace438f50c3b7540
joueur <lxp775399418@163.com>
2025-05-07 17:04:10 +0800

    main

--- src/threads1/li/xin/ping/Test.java
+++ src/threads1/li/xin/ping/Test.java
@@ -1,11 +1,13 @@
 package threads1.li.xin.ping;
-
+import java.lang.Thread;
 public class Test {
 
 //run subject at main thread
     public static void main(String... args){
-        TestThread tests1=new TestThread();
-        tests1.start();
+
+        ThreadTest1 test1=new ThreadTest1();
+        Thread th=new Thread(test1);
+        th.start();
 
     }
 
