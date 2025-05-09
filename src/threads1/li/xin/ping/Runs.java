582d8c220ba60fc8f4b010aa1424366d5c00f80a
joueur <lxp775399418@163.com>
2025-05-10 00:27:54 +0800

    main

--- src/threads1/li/xin/ping/Runs.java
+++ src/threads1/li/xin/ping/Runs.java
@@ -8,28 +8,26 @@ public class Runs implements Runnable{ //run方法调用runnable接口
     public Runs(){
 
     }
+
     Object obj=new Object();
-    public void run(){
-        synchmothed();
 
-    }
+    //*****重写run**
+    @Override
+    public void run(){synchmothed();}
 
+
+    //同步方法 ，是同步代码块的一种变体
     public synchronized void synchmothed(){
         while(true){
-
                 if (count > 0) {
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+                    try
+                    {Thread.sleep(50);}
+                    catch (InterruptedException e)
+                    {throw new RuntimeException(e);}
                     count--;
-
                     out.println(Thread.currentThread().getName() + "卖出了1张票，现在还剩" + count + "张票");
 
-                } else {
-                    break;
-                }
+                } else { break;}
         }
     }
 
