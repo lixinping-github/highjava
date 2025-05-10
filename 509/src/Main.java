da9762d23736a7e5d166be7b5b4c0c8fdbe355c6
joueur <lxp775399418@163.com>
2025-05-10 10:20:57 +0800

    main

--- 509/src/Main.java
+++ 509/src/Main.java
@@ -12,19 +12,19 @@ public class Main {
 
 class Lanhan{
     //设置懒汉类实例字段
-    private Lanhan lan=null;
+    private static Lanhan lan=null;
     //私有化懒汉
     private Lanhan(){}
     //写一个方法，懒汉方法设置lanhan类
-    public Lanhan getlanhan(){
+    public static synchronized Lanhan getlanhan(){
         //不安全问题 当多个线程同时调用此方法，会导致此方法有可能if被执行多次，导致私有字段lan被多次赋值
 
-        synchronized (Lanhan.class){ //此时为线程安全
+         //此时为线程安全
          if (lan == null){
             lan = new Lanhan();
          }
             return lan;
-        }
+
 
 
 
