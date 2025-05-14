d469d71959ad5f7703958b66de647d9a8f0bee3c
joueur <lxp775399418@163.com>
2025-05-14 16:30:56 +0800

    main

--- 513/src/StringTest.java
+++ 513/src/StringTest.java
@@ -2,12 +2,14 @@ import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 import org.junit.Test;
 import sun.nio.cs.ext.GBK;
 
+import javax.xml.transform.Result;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class StringTest {
 
-    String str="abc213嘻哈";//321cba  a21cb3
+//    String str="abc213嘻哈";//321cba  a21cb3
 
 
     @Test
@@ -42,9 +44,11 @@ public class StringTest {
 //        }
 //        System.out.println(strs);
 
+//模拟一个trim方法
+        String str="1111";
 
-
-
+        System.out.println(str);
+        System.out.println(getCount(str,""));
 
 
 
@@ -56,4 +60,66 @@ public class StringTest {
 
 
     }
+    //*
+    //模仿原生方法trim
+    // /
+
+    public String trimTest(String str){
+            char[] chars= str.toCharArray();
+            String result=new String(chars);
+
+            if(chars[0]==' '){
+                result=new String(Arrays.copyOfRange(chars,1,chars.length));
+
+                    if(chars[chars.length-1] == ' '){
+                        result=new String(Arrays.copyOfRange(result.toCharArray(),0,result.length()-1));
+                    }
+                }else if(chars[chars.length-1] == ' '){
+                result=new String(Arrays.copyOfRange(result.toCharArray(),0,result.length()-1));
+            }
+
+
+            return result;
+        }
+        //翻转一个字符串，将字符串中指定部分进行翻转，第一个参数为传入的字符串，
+        //第二个参数为翻转起始位置
+         //第三个参数为末尾位置，不包含
+        public String reserveTest(String str,int n1,int n2){
+            String result=str;
+            char[] splitArr=result.toCharArray();
+            char[] itemssp=Arrays.copyOfRange(splitArr,n1,n2);
+            for (int i=itemssp.length-1;i>=0;i--){
+                splitArr[n1]=itemssp[i];
+                n1++;
+            }
+            result=new String(splitArr);
+            return result;
+        }
+
+        public String getcount(String str,char a){
+
+        return str;
+    }
+
+    //获取一个字符串在另一个字符串中出现的次数；
+    public int getCount(String str1,String str2){
+        int result=0;
+        char[] charitems1=str1.toCharArray();
+        char[] charitems2=str2.toCharArray();
+        for(int i=0;i<charitems1.length-charitems2.length+1;i++){  //至关重要的一步
+            boolean flag=false;
+            for(int j=0;j<charitems2.length;j++){
+                if(charitems2[j] == charitems1[i+j]){
+                }else{
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                result++;
+            }
+        }
+            return result;
+    }
+
 }
