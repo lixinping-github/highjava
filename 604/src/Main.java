0f46b589e1dea6cf06da759de3ecd59a033eb593
lixinping <lxp775399418@163.com>
2025-06-04 17:54:31 +0800

    main

--- 604/src/Main.java
+++ 604/src/Main.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
@@ -22,15 +23,16 @@ public class Main {
      */
     @Test
     public void test1(){
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        updateList(list);
+        System.out.println(list);
 
-       ArrayList list =new ArrayList();
-       list.add(1);
-       list.add("张三");
-       list.add(false);
-       list.add(1);
-       list.add(1,true);
-       list.set(0,"李四");
+    }
 
-       System.out.println(list.subList(0,2));
+    private static void updateList(List list){
+        list.remove(2);
     }
 }
